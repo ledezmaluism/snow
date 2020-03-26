a7d4288ccbc4b27486c9969c4c0e951a2943aa29
Ledezma Hernandez <lmledezm@ndc.nasa.gov>
2020-03-26 09:40:19 -0700

    Create util.py

---
+++ Notebooks/util.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 11:19:37 2020
+
+@author: ledezmaluism
+
+Helper module to import Modules/layout.py
+"""
+
+# Import a module with the same name from a different directory.
+
+import importlib
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../Modules'))
+
+if not hasattr(importlib, 'reload'):
+    importlib.reload = reload  # for Python 2 compatibility
+
+# Temporarily hijack __file__ to avoid adding names at module scope;
+# __file__ will be overwritten again during the reload() call.
+__file__ = {'sys': sys, 'importlib': importlib}
+
+del importlib
+del os
+del sys
+
+__file__['importlib'].reload(__file__['sys'].modules[__name__])
