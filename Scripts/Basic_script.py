a154234c0ec955ca18563635db184eb4cb6b1274
Luis Ledezma <ledezmaluism@gmail.com>
2020-06-03 16:55:53 -0700

    Update Basic_script.py

--- Scripts/Basic_script.py
+++ Scripts/Basic_script.py
@@ -27,7 +27,7 @@ Load template:
 '''
 if 'mode' not in vars():
     lumapi = imp.load_source("lumapi", "C:/Program Files/Lumerical/2020a/api/python/lumapi.py")
-    mode = lumapi.MODE("Template_Luis.lms")
+    MODE = lumapi.MODE("Template_Luis.lms")
 
 '''
 Units
@@ -40,9 +40,9 @@ THz = 1e12
 Input parameters
 '''
 wavelength = 1*um
-h_LN = 0.7*nm
-h_etch = 0.2*nm
-w_ridge = 1000*nm
+h_LN = 700*nm
+h_etch = 250*nm
+w_ridge = 1300*nm
 h_slab = h_LN - h_etch
 
 theta = 60
@@ -56,19 +56,16 @@ print('width at the base = %.3f um' %(w_ridge_base))
 Simulation volume
 '''
 w_slab = 10*wavelength + 2*w_ridge
-# w_slab = 20*wavelength + 2*w_ridge
 h_margin = 4*wavelength
 h_substrate = 4*wavelength
-# h_substrate = 8*wavelength
 meshsize = wavelength/20
-# finemesh = wavelength/40
-finemesh = wavelength/80
+finemesh = wavelength/40
 
 '''
 Materials
 '''
-# material_substrate = "SiO2_analytic"
-material_substrate = "Sapphire_analytic"
+material_substrate = "SiO2_analytic"
+# material_substrate = "Sapphire_analytic"
 
 # material_thinfilm = "LN_analytic_undoped_xne"
 #material_thinfilm = "LN_analytic_undoped_zne"
@@ -79,8 +76,8 @@ material_thinfilm = "LN_analytic_MgO_doped_xne"
 '''
 Drawing and setup
 '''
-lum.draw_wg(mode, material_thinfilm, material_substrate,
+lum.draw_wg(MODE, material_thinfilm, material_substrate,
               h_LN, h_substrate, h_etch, w_ridge, w_slab, theta, wg_length)
-lum.add_fine_mesh(mode, finemesh, h_LN, w_ridge_base, x_factor=1.2, y_factor=1.5)
-lum.add_2D_mode_solver(mode, meshsize, h_LN, h_substrate, 
-                         w_slab, wg_length, h_margin)
+lum.add_fine_mesh(MODE, finemesh, h_LN, w_ridge_base, x_factor=1.2, y_factor=1.5)
+lum.add_2D_mode_solver(MODE, meshsize, h_LN, h_substrate, 
+                         w_slab, wg_length, h_margin, symmetry='TE')
