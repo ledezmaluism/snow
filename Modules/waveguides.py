009c6e0bd94fd97de1890352953da56bca7d52be
Ledezma <lmledezm@ndc.nasa.gov>
2020-06-25 10:57:57 -0700

    Update waveguides.py
    Added behavioral modeling of waveguides.

--- Modules/waveguides.py
+++ Modules/waveguides.py
@@ -64,6 +64,66 @@ class waveguide:
         df = f_abs[1] - f_abs[0]
         self.beta_1 = fftshift(np.gradient(fftshift(self.beta), 2*pi*df))
         
+    def behavioral(self, wl, z_wl, wl_1, wl_2, GVM=0, n_ff=2.0, wl_ff=None):
+        '''
+        This method is equivalent to "add_narray", but instead of using 
+        the physical waveguide parameters to calculate the behavior of the 
+        waveguide, it defines the behavior directly 
+        (so it defines a black box model of the waveguide)
+        Parameters
+        ----------
+        wl : TYPE
+            Wavelength array
+        z_wl : TYPE
+            Zero GVD wavelength
+        wl_1 : TYPE
+            First wavelegnth for GVM calculation
+        wl_2 : TYPE
+            Second wavelegnth for GVM calculation
+        GVM : TYPE, optional
+            GVM between wl_1 and wl_2. The default is 0.
+        n_ff : TYPE, optional
+            Refractive index at wl_ff. The default is 2.0.
+        wl_ff : TYPE, optional
+            Fundamental frequency for refractive index. The default is None.
+
+        Returns
+        -------
+        Waveguide object.
+
+        '''
+        if wl_ff == None:
+            wl_ff = ( wl_1 + wl_2 )/2
+        
+        #z1, w1, w2 are wavelengths
+        z1 = 2*pi*c/z_wl
+        omega_1 = 2*pi*c/wl_1
+        omega_2 = 2*pi*c/wl_2        
+        omega0_ff = 2*pi*c/wl_ff
+        
+        #Frequency array:
+        omega = 2*pi*c/wl
+        
+        #Constants
+        c1 = 10 **(-55) #s^4/m
+        
+        # First we want to determine the remianing free parameters
+        z2_num = 6*GVM - 2*c1*(omega_2**3-omega_1**3)+3*c1*z1*(omega_2**2-omega_1**2)
+        z2_den = 3*c1*(2*z1-omega_2-omega_1)*(omega_2-omega_1)
+        z2 = z2_num/z2_den
+        
+        c2 = n_ff/c - c1*(omega0_ff**3/12 - (z1+z2)*(omega0_ff**2)/6 +z1*z2*omega0_ff/2)
+        
+        # Now we can solve for the remaing parameters
+        beta_2 = c1*(omega**2 -(z1+z2)*omega + z1*z2)
+        beta_1 = c1*(omega**3/3 - (z1+z2)*(omega**2)/2 + z1*z2*omega) + c2
+        beta = c1*(omega**4/12 - (z1+z2)*(omega**3)/6 + z1*z2*(omega**2)/2) + c2*omega  
+        n = c*( c1*(omega**3/12 - (z1+z2)*(omega**2)/6 + z1*z2*omega/2) + c2 )
+        
+        self.beta = beta
+        self.beta_1 = beta_1
+        self.neff_array = n
+        
     def set_length(self, L):
         self.L = L
         
