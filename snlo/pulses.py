d3101c96bd33004b44b52aceb67bce75132a28e1
robbymgray <52333516+robbymgray@users.noreply.github.com>
2020-07-27 16:25:13 -0700

    Update pulses.py

--- snlo/pulses.py
+++ snlo/pulses.py
@@ -99,7 +99,7 @@ def sech(t, Energy, FWHM, f0=0):
         AMPLITUDE ARRAY.
     '''
     Ppeak = 0.88*Energy/FWHM
-    x = np.sqrt(Ppeak) / np.cosh( 1.76 * t / FWHM )
+    x = np.sqrt(Ppeak) / np.cosh( 1.76 * t/ FWHM )
     return x * np.exp(1j*2*pi*f0*t)
 
 def noise(t, Npower):
@@ -153,6 +153,15 @@ def filter_signal(f_abs, X, f0, bw):
     _, h = scipy.signal.sosfreqz(sos, worN = X.size)
     filtered = ifft( X * fftshift(h) )
     return filtered, h
+
+def ideal_filter_signal(f_abs, X, f0, bw):
+    #Input in is the frequency domain already
+    f1 = f0 - bw/2
+    f2 = f0 + bw/2
+    h = np.zeros(X.size)
+    h[(f_abs >= f1) & (f_abs <= f2)] = 1 
+    filtered = ifft( X * h)
+    return filtered, fftshift(h)
 
 def energy_td(t, x):
     dt = t[1] - t[0]
@@ -396,6 +405,12 @@ def plot_ESD_dB_vs_wavelength(t, x, f0, label='Energy Spectral Density (dBJ / Hz
                               xlim=xlim, ylim=ylim, wl_unit=wl_unit)
     return ax
 
+def add_t_offset(pulse, t_offset):
+    p_spec = pulse.A
+    p_spec = np.multiply(p_spec,np.exp(1j*t_offset*pulse.Omega))
+    pulse.update_fd(p_spec)
+    return pulse
+
 class pulse:
     def __init__(self, t, x, wavelength, domain='Time'):
 
@@ -444,17 +459,15 @@ class pulse:
         if np.size(self.t) == np.size(a):
             self.a = a
             self.A = fft(a, self.NFFT)
-            self.update_param()
         else:
-            raise RuntimeError('Hmm, this pulse seems diffenrent. Cannot update.')
+            raise RuntimeError('Hmm, this pulse seems different. Cannot update.')
 
     def update_fd(self, A):
         if np.size(self.t) == np.size(A):
             self.A = A
             self.a = ifft(A, self.NFFT)
-            self.update_param()
         else:
-            raise RuntimeError('Hmm, this pulse seems diffenrent. Cannot update.') 
+            raise RuntimeError('Hmm, this pulse seems different. Cannot update.') 
 
     def energy_td(self):
         t = self.t
